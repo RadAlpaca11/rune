6429bd5c69a82c62f4caf8d639cf951ba8633198
RadAlpaca11 <mayahayden@icloud.com>
2026-05-31 19:01:32 -0700

    adding file read and numbered lines

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ struct Editor {
     cursor_y: u16,
     window_width: u16,
     window_height: u16,
+    buffer: Vec<String>,
+    scroll_y: usize,
 }
 
 impl Editor {
@@ -26,32 +28,30 @@ impl Editor {
             cursor_y: 0,
             window_width,
             window_height,
+            buffer: Vec::new(),
+            scroll_y: 0,
         }
     }
 
-    fn move_cursor(&mut self, press: KeyCode) {
-        match press {
-            KeyCode::Up => {
-                self.cursor_y = self.cursor_y.saturating_sub(1);
-            }
-            KeyCode::Down => {
-                if self.cursor_y < self.window_height - 2 { self.cursor_y += 1; }
-            }
-            KeyCode::Left => {
-                self.cursor_x = self.cursor_x.saturating_sub(1);
-            }
-            KeyCode::Right => {
-                if self.cursor_x < self.window_width - 1 { self.cursor_x += 1; }
-            }
-            _ => {}
-        }
+    fn load(&mut self, path: &str) -> io::Result<()> {
+        let contents = std::fs::read_to_string(path)?;
+        self.buffer = contents.lines().map(|line| line.to_string()).collect();
+        Ok(())
     }
 
     fn draw(&self, stdout: &mut impl Write) -> io::Result<()> {
         execute!(stdout, terminal::Clear(ClearType::All))?;
+        
+        let line_number_width = self.buffer.len().to_string().len() as u16 + 2;
+
         for row in 0..self.window_height - 1 {
             execute!(stdout, cursor::MoveTo(0, row))?;
-            write!(stdout, "~")?;
+            let buffer_line = row as usize + self.scroll_y;
+            if let Some(line) = self.buffer.get(buffer_line) {
+                write!(stdout, "{:>width$} {}", buffer_line + 1, line, width = (line_number_width - 2) as usize)?;
+            } else {
+                write!(stdout, "~")?;
+            }
         }
 
         execute!(
@@ -72,18 +72,39 @@ impl Editor {
         Ok(())
     }
 
+    fn move_cursor(&mut self, press: KeyCode) {
+        match press {
+            KeyCode::Up => {
+                self.cursor_y = self.cursor_y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                if self.cursor_y < self.window_height - 2 { self.cursor_y += 1; }
+            }
+            KeyCode::Left => {
+                self.cursor_x = self.cursor_x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                if self.cursor_x < self.window_width - 1 { self.cursor_x += 1; }
+            }
+            _ => {}
+        }
+    }
+
     fn run(&mut self, stdout: &mut impl Write) -> io::Result<()> {
         loop {
             self.draw(stdout)?;
-            if let Event::Key(KeyEvent { code, modifiers, .. }) = event::read()? {
-                match (code, modifiers) {
-                    (KeyCode::Char('c'), KeyModifiers::CONTROL) => break, // TODO: prompt to save if there are unsaved changes
-                    (code, _) => self.move_cursor(code),
+            match event::read()? {
+                Event::Key(KeyEvent { code, modifiers, .. }) => {
+                    match (code, modifiers) {
+                        (KeyCode::Char('c'), KeyModifiers::CONTROL) => break,
+                        (code, _) => self.move_cursor(code),
+                    }
                 }
-            }
-            if let Event::Resize(width, height) = event::read()? {
-                self.window_width = width;
-                self.window_height = height;
+                Event::Resize(width, height) => {
+                    self.window_width = width;
+                    self.window_height = height;
+                }
+                _ => {}
             }
         }
         Ok(())
@@ -95,6 +116,8 @@ fn main() -> io::Result<()> {
     terminal::enable_raw_mode()?;
     execute!(stdout, EnterAlternateScreen)?;
     let mut rune = Editor::new();
+    let args: Vec<String> = std::env::args().collect();
+    rune.load(&args[1])?;
     rune.run(&mut stdout)?;
     execute!(stdout, LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
